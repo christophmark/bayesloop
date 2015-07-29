a0a88f4af5284e000655a08aa6928a6377eee9b4
Christoph Mark <christoph.mark@fau.de>
2015-07-29 15:57:31 +0200

    Added support for missing data inline comments according to PEP8

--- bayesloop/observationModel.py
+++ bayesloop/observationModel.py
@@ -8,9 +8,10 @@ class Poisson:
     def __init__(self):
         self.name = 'Poisson'
 
-        self.segmentLength = 1 # number of measurements in one data segment
+        self.segmentLength = 1  # number of measurements in one data segment
         self.defaultGridSize = [1000]
         self.defaultBoundaries = [[0, 1]]
+        self.uninformativePdf = None
 
     def pdf(self, grid, x):
         """
@@ -20,4 +21,12 @@ class Poisson:
         :param x: data segment from formatted data
         :return: poisson pdf (shape like grid)
         """
+
+        # check for missing data
+        if np.isnan(x[0]):
+            if self.uninformativePdf is not None:
+                return self.uninformativePdf  # arbitrary likelihood
+            else:
+                return np.ones_like(grid[0])/np.sum(np.ones_like(grid[0]))  # uniform likelihood
+
         return (grid[0]**x[0])*(np.exp(-grid[0]))/(np.math.factorial(x[0]))
