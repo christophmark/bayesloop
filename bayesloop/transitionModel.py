6b5e7969000b57d6bc0576a22fa9b30dc297d1f2
Christoph Mark <christoph.mark@fau.de>
2015-08-05 15:05:30 +0200

    Added support for combined transition models.

--- bayesloop/transitionModel.py
+++ bayesloop/transitionModel.py
@@ -5,6 +5,7 @@ transitionModel.py introduces models for parameter variations.
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter1d
 
+
 class Static:
     def __init__(self):
         self.latticeConstant = None
@@ -23,7 +24,8 @@ class Static:
         return posterior.copy()
 
     def computeBackwardPrior(self, posterior, t):
-        return self.computeForwardPrior(posterior, t-1)
+        return self.computeForwardPrior(posterior, t - 1)
+
 
 class GaussianRandomWalk:
     def __init__(self, sigma=None):
@@ -42,12 +44,13 @@ class GaussianRandomWalk:
         :return: grid-like prior distribution
         """
         newPrior = posterior.copy()
-        newPrior = gaussian_filter1d(newPrior, self.sigma/self.latticeConstant[0])
+        newPrior = gaussian_filter1d(newPrior, self.sigma / self.latticeConstant[0])
 
         return newPrior
 
     def computeBackwardPrior(self, posterior, t):
-        return self.computeForwardPrior(posterior, t-1)
+        return self.computeForwardPrior(posterior, t - 1)
+
 
 class ChangePoint:
     def __init__(self, tChange=None):
@@ -66,12 +69,13 @@ class ChangePoint:
         :return: grid-like prior distribution
         """
         if t == self.tChange:
-            return np.ones_like(posterior)/np.sum(np.ones_like(posterior))  # return flat distribution
+            return np.ones_like(posterior) / np.sum(np.ones_like(posterior))  # return flat distribution
         else:
             return posterior.copy()
 
     def computeBackwardPrior(self, posterior, t):
-        return self.computeForwardPrior(posterior, t-1)
+        return self.computeForwardPrior(posterior, t - 1)
+
 
 class RegimeSwitch:
     def __init__(self, pMin=None):
@@ -95,4 +99,38 @@ class RegimeSwitch:
         return newPrior
 
     def computeBackwardPrior(self, posterior, t):
-        return self.computeForwardPrior(posterior, t-1)
+        return self.computeForwardPrior(posterior, t - 1)
+
+
+class CombinedTransitionModel:
+    def __init__(self, *args):
+        self.latticeConstant = None
+        self.models = args
+
+    def __str__(self):
+        return 'Combined transition model'
+
+    def computeForwardPrior(self, posterior, t):
+        """
+        Compute new prior from old posterior.
+
+        :param posterior: grid-like posterior distribution
+        :param t: integer time step
+        :return: grid-like prior distribution
+        """
+        newPrior = posterior.copy()
+
+        for m in self.models:
+            m.latticeConstant = self.latticeConstant  # latticeConstant needs to be propagated to sub-models
+            newPrior = m.computeForwardPrior(newPrior, t)
+
+        return newPrior
+
+    def computeBackwardPrior(self, posterior, t):
+        newPrior = posterior.copy()
+
+        for m in self.models:
+            m.latticeConstant = self.latticeConstant
+            newPrior = m.computeBackwardPrior(newPrior, t)
+
+        return newPrior
