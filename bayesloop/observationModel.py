fc4fcc502cd9002d384e001e644d11436319eb6b
Christoph Mark <christoph.mark@fau.de>
2015-08-16 17:33:14 +0200

    Added autoregressive process of first order (AR1) as observation model.

--- bayesloop/observationModel.py
+++ bayesloop/observationModel.py
@@ -31,3 +31,31 @@ class Poisson:
                 return np.ones_like(grid[0])/np.sum(np.ones_like(grid[0]))  # uniform likelihood
 
         return (grid[0]**x[0])*(np.exp(-grid[0]))/(np.math.factorial(x[0]))
+
+class AR1:
+    def __init__(self):
+        self.segmentLength = 2  # number of measurements in one data segment
+        self.defaultGridSize = [200, 200]
+        self.defaultBoundaries = [[-1, 1], [0, 1]]
+        self.uninformativePdf = None
+
+    def __str__(self):
+        return 'Autoregressive process of first order (AR1)'
+
+    def pdf(self, grid, x):
+        """
+        Probability density function of the observational model
+
+        :param grid: parameter grid
+        :param x: data segment from formatted data
+        :return: pdf for the autoregressive process of first order (shape like grid)
+        """
+
+        # check for missing data
+        if np.isnan(x[0]) or np.isnan(x[1]):
+            if self.uninformativePdf is not None:
+                return self.uninformativePdf  # arbitrary likelihood
+            else:
+                return np.ones_like(grid[0])/np.sum(np.ones_like(grid[0]))  # uniform likelihood
+
+        return np.exp(-((x[1] - grid[0]*x[0])**2.)/(2.*grid[1]**2.) - np.log(2.*np.pi*grid[1]**2.))
