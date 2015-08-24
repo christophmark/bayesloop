75c26cdd187c30018168c8ed4ba094c524fc56fb
Christoph Mark <christoph.mark@fau.de>
2015-08-24 11:56:48 +0200

    set version: 0.1; adjusted setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-setup.py
-"""
 
 from setuptools import setup
 
-setup(name='bayesloop',
-      version='0.0',
-      description='Sequential Bayesian Inference Algorithm',
-      url='none',
-      author='Christoph W. Mark',
-      author_email='christoph.mark@fau.de',
-      license='none',
-      packages=['bayesloop'],
-      zip_safe=False)
+setup(
+    name='bayesloop',
+    version='0.1',
+    description='Framework for fitting hierarchical time series models based on Bayesian inference.',
+    url='https://bitbucket.org/chrismark/bayesloop',
+    author='Christoph Mark',
+    author_email='christoph.mark@fau.de',
+    license='The MIT License (MIT)',
+    packages=['bayesloop'],
+    install_requires=['numpy>=1.9.2', 'scipy>=0.15.1']
+    )
