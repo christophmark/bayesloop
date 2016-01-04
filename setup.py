413809a638d5ab30f0d8678287d79a7e0d20ecdf
Christoph Mark <christoph.mark@fau.de>
2016-01-04 19:22:56 +0100

    Updated version: 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='bayesloop',
-    version='0.4',
+    version='0.5',
     description='Framework for fitting hierarchical time series models based on Bayesian inference.',
     url='https://bitbucket.org/chrismark/bayesloop',
     author='Christoph Mark',
