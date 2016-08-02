8ed9669436b87c766083aef2d1e0b8b56650b605
Christoph Mark <christoph.mark@fau.de>
2016-08-02 13:36:01 +0200

    Cleaner way of defining transition models; fixed plotting

--- bayesloop/onlineStudy.py
+++ bayesloop/onlineStudy.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 """
 This file introduces an extension to the basic Study-class which allows to add data step by step and keep an updated
-parameter distribution, allowing to analyze data streams online.
+parameter distribution, enabling the analysis of on-going data streams.
 """
 
 from __future__ import division, print_function
 import numpy as np
 from .study import Study
-import matplotlib.pyplot as plt
-from .helper import create_colormap
+from collections import OrderedDict, Iterable
 
 
 class OnlineStudy(Study):
     """
     This class builds on the Study-class and features a step-method to include new data points in the study as they
     arrive from a data stream. This online-analysis is performed in an forward-only way, resulting in filtering-
-    distributions only. If one is interested in smoothing-distributions, use the conventional fit-method.
+    distributions only. In contrast to a normal study, however, one can add multiple transition models to account for
+    different types of parameter dynamics (similar to a Hyper study). The Online study then computes the probability
+    distribution over all transition models for each new data point, enabling real-time model selection.
 
-    Note: Only the complete rawData is preserved; all results are only current results and have to be stored by external
-        functions to be used later.
+    Args:
+        storeHistory: If true, posterior distributions and their mean values, as well as hyper-posterior distributions
+            are stored for all time steps.
     """
     def __init__(self, storeHistory=False):
         super(OnlineStudy, self).__init__()
@@ -26,6 +28,8 @@ class OnlineStudy(Study):
         self.transitionModels = None
         self.tmCount = None
         self.tmCounts = None
+        self.hyperParameters = None
+        self.selectedHyperParameters = None
         self.alpha = None
         self.beta = None
         self.normi = None
@@ -41,29 +45,58 @@ class OnlineStudy(Study):
         self.posteriorSequence = []
         self.hyperPosteriorSequence = []
         self.parameterPosteriorSequence = []
-        print('  --> Loops model.')
+        print('  --> Online study')
 
     def addTransitionModel(self, transitionModel):
         """
-        TODO
+        Adds a transition model to the list of transition models that are fitted in each time step. Note that a list of
+        hyper-parameter values can be supplied.
 
         Args:
-            transitionModel: TODO
+            transitionModel: instance of a transition model class.
+
+        Example:
+            Here, 'S' denotes the OnlineStudy instance. In the first example, we assume a Poisson observation model and
+            add a Gaussian random walk with varying standard deviation to the rate parameter 'lambda':
+
+                S.setObservationModel(bl.om.Poisson())
+                S.addTransitionModel(bl.tm.GaussianRandomWalk(sigma=[0, 0.1, 0.2, 0.3], param='lambda'))
         """
+        if str(transitionModel) == 'Serial transition model':
+            print('    ! Serial transition models are not supported by OnlineStudy.')
+            return
+        if str(transitionModel) == 'Combined transition model':
+            print('    ! Combined transition models are not supported by OnlineStudy.')
+            return
 
         if self.transitionModels is None:
-            self.transitionModels = [transitionModel]
-        else:
-            self.transitionModels.append(transitionModel)
+            self.transitionModels = []
+            self.hyperParameters = []
+            self.selectedHyperParameters = []
+
+        self.transitionModels.append(transitionModel)
+        self.hyperParameters.append(transitionModel.hyperParameters.copy())
+
+        # if a single hyper-parameter value is provided, pack it in a list
+        for key, values in self.hyperParameters[-1].items():
+            if not isinstance(values, Iterable):
+                self.hyperParameters[-1][key] = [values]
+
+        try:
+            self.selectedHyperParameters.append(transitionModel.selectedHyperParameter)
+        except:  # transition model does not support the selection of a specific hyper-parameter
+            self.selectedHyperParameters.append(None)
 
         # set first hypothesis as transition model (so consistency check of bayesloop passes)
-        self.setTransitionModel(self.transitionModels[0][0](self.transitionModels[0][1][0]), silent=True)
+        self.setTransitionModel(self.transitionModels[0], silent=True)
 
         # count individual transition models
         self.tmCounts = []
-        for tm in self.transitionModels:
-            self.tmCounts.append(len(tm[1]))
+        for i, tm in enumerate(self.transitionModels):
+            self.tmCounts.append(len(self.hyperParameters[i].values()[0]))
         self.tmCount = np.sum(self.tmCounts)
+
+        print('    + Added transition model: {}'.format(transitionModel))
 
     def step(self, dataPoint):
         """
@@ -72,12 +105,22 @@ class OnlineStudy(Study):
         Args:
             dataPoint: Float, int, or 1D-array of those (for multidimensional data).
         """
+        # at least one transition model has to be set or added
+        if (self.tmCount is None) and (self.transitionModel is None):
+            print('    ! No transition model set or added.')
+            return
+
+        # if one only sets a transition model, but does not use addTransitionModel, we add it here
+        if (self.tmCount is None) and (self.transitionModel is not None):
+            self.addTransitionModel(self.transitionModel)
+
         if not isinstance(dataPoint, list):
             dataPoint = [dataPoint]
 
         if len(self.rawData) == 0:
             self.rawData = np.array(dataPoint)
             self.rawTimestamps = np.array([0])
+            self.formattedTimestamps = []
         else:
             self.rawData = np.append(self.rawData, np.array(dataPoint), axis=0)
             self.rawTimestamps = np.append(self.rawTimestamps, self.rawTimestamps[-1]+1)
@@ -86,6 +129,8 @@ class OnlineStudy(Study):
         if len(self.rawData) < self.observationModel.segmentLength:
             print('    ! Not enough data points to start analysis. Will wait for more data.')
             return
+
+        self.formattedTimestamps.append(self.rawTimestamps[-1])
 
         # initialize hyper-prior as flat
         if self.hyperPrior is None:
@@ -121,11 +166,22 @@ class OnlineStudy(Study):
 
         # loop over all hypotheses
         idx = 0
-        for tm in self.transitionModels:
-            model = tm[0]
-            params = tm[1]
-            for p in params:
-                self.setTransitionModel(model(p), silent=True)
+        for tm, hp, sp in zip(self.transitionModels, self.hyperParameters, self.selectedHyperParameters):
+
+            for j in range(len(hp.values()[0])):  # loop over all hyper-parameter values to fit
+                # create dictionary with keyword-arguments to pass to transition model
+                odict = OrderedDict()
+                for param, value in hp.items():
+                    odict[param] = value[j]
+                tm.hyperParameters = odict
+
+                try:
+                    tm.selectedHyperParameter = sp
+                except:  # transition model does not support the selection of a specific hyper-parameter
+                    pass
+
+                # set current transition model and hyper-parameter values
+                self.setTransitionModel(tm, silent=True)
 
                 # compute alpha_i
                 alphai = self.transitionModel.computeForwardPrior(self.alpha, len(self.formattedData)-1)*likelihood
@@ -143,6 +199,7 @@ class OnlineStudy(Study):
                 # update normalization constant
                 self.normi[idx] = ni
 
+                print(idx)
                 idx += 1
 
         # normalize hyper-posterior
@@ -152,7 +209,7 @@ class OnlineStudy(Study):
         self.marginalizedPosterior /= np.sum(self.marginalizedPosterior)
 
         # compute new alpha
-        self.alpha = self.marginalizedPosterior  # *np.sum(self.normi)
+        self.alpha = self.marginalizedPosterior
 
         # store results for future plotting
         if self.storeHistory:
@@ -160,73 +217,25 @@ class OnlineStudy(Study):
             self.posteriorSequence.append(self.marginalizedPosterior.copy())
             self.hyperPosteriorSequence.append(self.hyperPosterior.copy())
 
-    def plotParameterEvolution(self, param=0, xLower=None, xUpper=None, color='b', gamma=0.5, **kwargs):
+    def plotParameterEvolution(self, param=0, color='b', gamma=0.5, **kwargs):
         """
         Plots a series of marginal posterior distributions corresponding to a single model parameter, together with the
         posterior mean values.
 
         Args:
             param: parameter name or index of parameter to display; default: 0 (first model parameter)
-
             color: color from which a light colormap is created
-
             gamma: exponent for gamma correction of the displayed marginal distribution; default: 0.5
-
-            **kwargs: all further keyword-arguments are passed to the plot of the posterior mean values
+            kwargs: all further keyword-arguments are passed to the plot of the posterior mean values
         """
-        posteriorSequence = np.array(self.posteriorSequence)
-        posteriorMeanValues = np.array(self.posteriorMeanValues).T
+        # plotting function of Study class can only handle arrays, not lists
+        self.formattedTimestamps = np.array(self.formattedTimestamps)
+        self.posteriorMeanValues = np.array(self.posteriorMeanValues).T
+        self.posteriorSequence = np.array(self.posteriorSequence)
 
-        if self.posteriorSequence == []:
-            print('! Cannot plot posterior sequence as it has not yet been computed. Run complete fit.')
-            return
+        Study.plotParameterEvolution(self, param=0, color='b', gamma=0.5, **kwargs)
 
-        if isinstance(param, int):
-            paramIndex = param
-        elif isinstance(param, str):
-            paramIndex = -1
-            for i, name in enumerate(self.observationModel.parameterNames):
-                if name == param:
-                    paramIndex = i
-
-            # check if match was found
-            if paramIndex == -1:
-                print('! Wrong parameter name. Available options: {0}'.format(self.observationModel.parameterNames))
-                return
-        else:
-            print('! Wrong parameter format. Specify parameter via name or index.')
-            return
-
-        axesToMarginalize = list(range(1, len(self.observationModel.parameterNames) + 1))  # axis 0 is time
-        axesToMarginalize.remove(paramIndex + 1)
-
-        marginalPosteriorSequence = np.squeeze(np.apply_over_axes(np.sum, posteriorSequence, axesToMarginalize))
-
-        if not xLower:
-            xLower = 0
-        if not xUpper:
-            if xLower:
-                print('! If lower x limit is provided, upper limit has to be provided, too.')
-                print('  Setting lower limit to zero.')
-            xLower = 0
-            xUpper = len(marginalPosteriorSequence)
-
-        plt.imshow((marginalPosteriorSequence.T) ** gamma,
-                   origin=0,
-                   cmap=create_colormap(color),
-                   extent=[xLower, xUpper - 1] + self.boundaries[paramIndex],
-                   aspect='auto')
-
-        # set default color of plot to black
-        if (not 'c' in kwargs) and (not 'color' in kwargs):
-            kwargs['c'] = 'k'
-
-        # set default linewidth to 1.5
-        if (not 'lw' in kwargs) and (not 'linewidth' in kwargs):
-            kwargs['lw'] = 1.5
-
-        plt.plot(np.arange(xLower, xUpper), posteriorMeanValues[paramIndex], **kwargs)
-
-        plt.ylim(self.boundaries[paramIndex])
-        plt.ylabel(self.observationModel.parameterNames[paramIndex])
-        plt.xlabel('time step')
+        # re-transform arrays to lists, so online study may continue to append values
+        self.formattedTimestamps = list(self.formattedTimestamps)
+        self.posteriorMeanValues = list(self.posteriorMeanValues)
+        self.posteriorSequence = list(self.posteriorSequence)
