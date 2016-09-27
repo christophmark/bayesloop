b17410891931d0c657612aeac72eb29c78937e37
Christoph Mark <christoph.mark@fau.de>
2016-09-27 11:18:46 +0200

    Combined all studies in one file

--- bayesloop/__init__.py
+++ bayesloop/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from .study import *
-from .changepointStudy import *
-from .hyperStudy import *
-from .onlineStudy import *
-from .jeffreys import *
-from .fileIO import *
+# import study types
+from .core import *
 
 # observation models and transition models need to be distinguishable
 from . import observationModels
 from . import observationModels as om  # short form
 from . import transitionModels
 from . import transitionModels as tm  # short form
+
+# misc
+from .jeffreys import *
+from .fileIO import *
 
