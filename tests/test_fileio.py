f15d09ba2d4a03525d6b463645dceb137f074232
Christoph Mark <christoph.mark@fau.de>
2017-09-15 16:02:16 +0200

    Fixed typo

--- tests/test_fileio.py
+++ tests/test_fileio.py
@@ -7,7 +7,6 @@ import numpy as np
 
 class TestFileIO:
     def test_save_load(self):
-        # carry out fit (this test is designed to fall back on the fit method of the Study class)
         S = bl.HyperStudy()
         S.loadData(np.array([1, 2, 3, 4, 5]))
         S.setOM(bl.om.Gaussian('mean', bl.cint(0, 6, 20), 'sigma', bl.oint(0, 2, 20)))
