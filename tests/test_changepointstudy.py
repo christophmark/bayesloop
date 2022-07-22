887af5e4d1cd59a8d4c0ec3a83098d1d6d96865f
Christoph Mark <christoph.mark@fau.de>
2022-07-22 17:32:29 +0200

    fix again

--- tests/test_changepointstudy.py
+++ tests/test_changepointstudy.py
@@ -75,7 +75,7 @@ class TestTwoParameterModel:
 
         # test parameter distributions
         np.testing.assert_allclose(S.getParameterDistributions('mean', density=False)[1][:, 5],
-                                   [0.03372851, 0.05087598, 0.02024129, 0.00020918, 0.00020918],
+                                   [0.033729, 0.050869, 0.020636, 0.001647, 0.001647],
                                    rtol=1e-02, err_msg='Erroneous posterior distribution values.')
 
         # test parameter mean values
